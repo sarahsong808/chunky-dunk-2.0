857640151f84f4fa18fa117ff42bb1a1ecc9cae7
Sarah's Mac <sarahsongg@yahoo.com>
2020-07-28 19:48:34 -0700

    connected to api key

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+  const APP_ID = "94743f0c";
+  const APP_KEY = "01451fab7300ac70516120de63f295cd";
+
+  const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  
   return (
     <div className="App">
       <h1> Chunky Dunk </h1>
