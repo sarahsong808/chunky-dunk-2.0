3c6fd2d312d477b1c566924465b115c5e6ae73cc
Sarah's Mac <sarahsongg@yahoo.com>
2020-07-28 23:21:58 -0700

    ingredients display toggle

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RecipeDetails from './RecipeDetails';
 
 const Recipe = ({recipe}) => {
+    const [display, setDisplay] = useState(false);
 const { label, image, url, ingredients } = recipe.recipe;
 return (
     <div className="recipe">
         <h2>{label}</h2>
         <img src={image} alt={label}/>
         <a href={url} target="_blank" rel="noopener noreferrer"> URL </a>
-        <button>Ingredients</button>
+        <button onClick={() => setDisplay(!display)}>Ingredients</button>
         <RecipeDetails ingredients={ingredients} />
     </div>
     );
