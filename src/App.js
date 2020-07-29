b9a1815b2ca29022573caee72f90b2b5db8207ce
Sarah's Mac <sarahsongg@yahoo.com>
2020-07-28 21:13:11 -0700

    search bar added

--- src/App.js
+++ src/App.js
@@ -6,14 +6,26 @@ function App() {
   const APP_ID = "94743f0c";
   const APP_KEY = "01451fab7300ac70516120de63f295cd";
 
-  const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const url = `https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-  const getData = () => {
-    
+  //wait for info before loading javascript 
+  const getData = async () => {
+    const result = await Axios.get(url);
+    console.log(result);
   }
+//once you click this button, you will reload page so preventDefault will stop this
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getData();
+  }
+
   return (
     <div className="App">
       <h1> Chunky Dunk </h1>
+      <form className="search-form" onSubmit={onSubmit}>
+        <input type="text" placeholder="Search Recipes" autoComplete="off" />
+        <input type="submit" value="search" />
+      </form>
     </div>
   );
 }
